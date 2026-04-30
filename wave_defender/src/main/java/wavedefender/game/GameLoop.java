11d12f539c56533df059d4aaae558000eb790088
t1po2 <nv_hung05@yahoo.com>
2026-05-01 01:36:05 +0200

    [Late Night Coding] Time: 1:35

--- wave_defender/src/main/java/wavedefender/game/GameLoop.java
+++ wave_defender/src/main/java/wavedefender/game/GameLoop.java
@@ -1,6 +1,23 @@
 package wavedefender.game;
 
-public class GameLoop {
+public class GameLoop implements Runnable{
+
+
+    private boolean running;
+    private final double updateRate = 1.0d/60.d;
+
+
+
+    // finally a helpful video https://www.youtube.com/watch?v=LICU6zH8gVU
+
+    // watch the video again make comments
+
+    @Override
+    public void run(){
+        running  = true;
+        double  accumulator = 0;
+        long curenTime;
+    }
 
     
     
