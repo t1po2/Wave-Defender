45a5bb860c3d7e68a62370fb14e2524a70d8a627
t1po2 <nv_hung05@yahoo.com>
2026-05-04 10:45:24 +0200

    IDK

--- wave_defender/src/main/java/wavedefender/game/GameLoop.java
+++ wave_defender/src/main/java/wavedefender/game/GameLoop.java
@@ -3,22 +3,44 @@ package wavedefender.game;
 public class GameLoop implements Runnable{
 
 
-    private boolean running;
-    private final double updateRate = 1.0d/60.d;
-
+    private boolean running; // am i running?
+    private final double updateRate = 1.0d/60.d; // Rate: 60 updates per sec 
 
 
     // finally a helpful video https://www.youtube.com/watch?v=LICU6zH8gVU
-
     // watch the video again make comments
-
     @Override
     public void run(){
-        running  = true;
-        double  accumulator = 0;
-        long curenTime;
+
+        running  = true;        
+        double  accumulator = 0;      //keeps track of time to update
+        long currentTime, lastUpdate = System.currentTimeMillis();
+
+
+
+        //actual gameloop : 
+        while(running){
+            currentTime = System.currentTimeMillis();
+            double lastRenderTimeInSec = (currentTime - lastUpdate) /1000d;
+            accumulator = lastRenderTimeInSec;
+            lastUpdate = currentTime;
+
+            while (accumulator > updateRate){
+                update();
+                
+                accumulator -= updateRate; 
+            }
+            render();
+        }
     }
 
-    
-    
+
+    private void render() {
+
+    }
+
+    private void update() {
+
+    }
+
 }
