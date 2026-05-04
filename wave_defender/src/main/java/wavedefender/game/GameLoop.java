12f5b282437b8fa842e35da02f77865a73ad7764
t1po2 <nv_hung05@yahoo.com>
2026-05-04 20:06:58 +0200

    Late Night Session

--- wave_defender/src/main/java/wavedefender/game/GameLoop.java
+++ wave_defender/src/main/java/wavedefender/game/GameLoop.java
@@ -6,6 +6,8 @@ public class GameLoop implements Runnable{
     private boolean running; // am i running?
     private final double updateRate = 1.0d/60.d; // Rate: 60 updates per sec 
 
+    private long nextStatTime;
+    private int fps, ups;   // frames per sec ; updates per sec
 
     // finally a helpful video https://www.youtube.com/watch?v=LICU6zH8gVU
     // watch the video again make comments
@@ -14,16 +16,17 @@ public class GameLoop implements Runnable{
 
         running  = true;        
         double  accumulator = 0;      //keeps track of time to update
-        long currentTime, lastUpdate = System.currentTimeMillis();
+        long currentTime;
+        long lastUpdate = System.currentTimeMillis();
 
 
 
         //actual gameloop : 
         while(running){
             currentTime = System.currentTimeMillis();
-            double lastRenderTimeInSec = (currentTime - lastUpdate) /1000d;
-            accumulator = lastRenderTimeInSec;
-            lastUpdate = currentTime;
+            double lastRenderTimeInSec = (currentTime - lastUpdate) /1000d; // calculates Gap bewtween initiated time and time until next Update
+            accumulator = lastRenderTimeInSec;   // displays lastRenderTimeInSec
+            lastUpdate = currentTime;   //prep for next loop 
 
             while (accumulator > updateRate){
                 update();
@@ -31,15 +34,27 @@ public class GameLoop implements Runnable{
                 accumulator -= updateRate; 
             }
             render();
+            printStats();
+        }
+    }
+
+
+
+
+    private void printStats(){
+        if ( System.currentTimeMillis() > nextStatTime) {
+            System.out.println(String.format("FPS: %d, UPS:  %d", fps,ups));
         }
     }
 
 
     private void render() {
+        fps++;
 
     }
 
     private void update() {
+        ups++;
 
     }
 
---
+++ wave_defender/src/main/java/wavedefender/game/Launcher.java
@@ -0,0 +1,11 @@
+package wavedefender.game;
+
+public class Launcher {
+
+    public static void main(String[] args){
+        new Thread(new GameLoop()).start();
+    }
+
+
+    
+}
