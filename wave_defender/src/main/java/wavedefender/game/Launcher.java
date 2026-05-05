4ac24341738c67bd4e4b7d4f45580e40b69da0ff
t1po2 <nv_hung05@yahoo.com>
2026-05-05 16:12:48 +0200

    I FUCKING DID IT WINDOW IS NOW REACTING

--- wave_defender/src/main/java/wavedefender/game/Launcher.java
+++ wave_defender/src/main/java/wavedefender/game/Launcher.java
@@ -1,15 +1,15 @@
 package wavedefender.game;
 
+import javax.swing.SwingUtilities;
+
 public class Launcher {
 
-
-
-
-  public static void main(String[] args){
-    System.out.println("Creating Window...");
-    Display display = new Display(800, 600);
-    System.out.println("Window created");
-  }
-
-
+    public static void main(String[] args) {
+        // This tells Java to run the GUI code on the Event Dispatch Thread
+        SwingUtilities.invokeLater(() -> {
+            System.out.println("Creating Window...");
+            Display display = new Display(800, 600);            
+            System.out.println("Window created and visible");
+        });
+    }
 }
