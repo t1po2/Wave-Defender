d0e783093359f2bb918906e0ccceabb429e002f7
t1po2 <nv_hung05@yahoo.com>
2026-05-05 15:25:04 +0200

    Cant Run Java Application Window on my Mac :(

--- wave_defender/src/main/java/wavedefender/game/Launcher.java
+++ wave_defender/src/main/java/wavedefender/game/Launcher.java
@@ -6,8 +6,9 @@ public class Launcher {
 
 
   public static void main(String[] args){
+    System.out.println("Creating Window...");
     Display display = new Display(800, 600);
-
+    System.out.println("Window created");
   }
 
 
