e1b4cf1f22d760522a6d96163f0cb5cd8310c801
t1po2 <nv_hung05@yahoo.com>
2026-05-05 14:53:47 +0200

    HELP ME

--- wave_defender/src/main/java/wavedefender/game/Launcher.java
+++ wave_defender/src/main/java/wavedefender/game/Launcher.java
@@ -2,12 +2,13 @@ package wavedefender.game;
 
 public class Launcher {
 
-    public static void main(String[] args){
-      Thread gameThread = new Thread(new GameLoop(new Game(800,600)));
-      gameThread.start();
-       
-    }
 
 
-    
+
+  public static void main(String[] args){
+    Display display = new Display(800, 600);
+
+  }
+
+
 }
